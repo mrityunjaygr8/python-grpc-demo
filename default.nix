56223dd16ef819d81122fd85f962dad67ac48c4f
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2023-03-09 17:32:45 +0530

    initial commit

---
+++ default.nix
@@ -0,0 +1,5 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [ python310Packages.poetry python310 fish ];
+}
